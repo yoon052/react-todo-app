c25a416b1e84f759c26ee761e4704ee70f1515b1
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-16 23:52:22 +0900

    feat: delete all button
    make setTodoData an empty array

--- src/App.js
+++ src/App.js
@@ -30,7 +30,13 @@ export default function App() {
 
     //원래 있던 할 일에 새로운 할일 더해주기
     setTodoData((prev) => [...prev, newTodo]);
+
+    //입력란에 있던 글씨 지워주기
     setValue("");
+  };
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
   };
 
   return (
@@ -38,6 +44,7 @@ export default function App() {
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4  lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <Lists
           handleClick={handleClick}
