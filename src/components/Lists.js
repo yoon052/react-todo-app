cd15d2dc2c7c9341bf89edcb021f0946598dea79
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-16 22:37:11 +0900

    feat: drag & drop API

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function List({ todoData, setTodoData }) {
   const handleCompletedChange = (id) => {
@@ -17,33 +18,72 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
+  const handleEnd = (result) => {
+    console.log("result", result);
+    if (!result.destination) return;
+
+    const newTodoData = todoData;
+
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+  };
+
   return (
     <div>
-      {todoData.map((data) => (
-        <div key={data.id}>
-          <div className="flex items-center justify-between w-full px-4 my-2 text-gray-600 bg-gray-100 border rounded">
-            <div className="items-center">
-              <input
-                type="checkbox"
-                defaultChecked={false}
-                onChange={() => handleCompletedChange(data.id)}
-              />
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      key={data.id}
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      className={`${
+                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+                      } flex items-center justify-between w-full px-4 my-2 text-gray-600 bg-gray-100 border rounded`}
+                    >
+                      <div className="items-center">
+                        <input
+                          type="checkbox"
+                          defaultChecked={false}
+                          onChange={() => handleCompletedChange(data.id)}
+                        />
 
-              <span className={data.completed ? "line-through" : undefined}>
-                {data.title}
-              </span>
+                        <span
+                          className={
+                            data.completed ? "line-through" : undefined
+                          }
+                        >
+                          {data.title}
+                        </span>
+                      </div>
+                      <div className="items-center">
+                        <button
+                          className="px-4 py-2 float-right"
+                          onClick={() => handleClick(data.id)}
+                        >
+                          x
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-            <div className="items-center">
-              <button
-                className="px-4 py-2 float-right"
-                onClick={() => handleClick(data.id)}
-              >
-                x
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
